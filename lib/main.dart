819b3bbb731f97ce41e5290c4766b660a0402a74
KorGan <daniil.king333@gmail.com>
2025-01-14 11:08:42 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:space_scutum_todo/ui/widgets/app/app.dart';
+
+void main() => runApp(const App());
---
+++ lib/ui/widgets/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Space Scutum ToDo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Center(child: Text('Space Scutum ToDo'))),
+    );
+  }
+}

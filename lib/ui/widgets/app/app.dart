cc32682785fd9616cc51e7335f07da93ec0f2c11
KorGan <daniil.king333@gmail.com>
2025-01-14 22:20:18 +0200

    Updated App.

--- lib/ui/widgets/app/app.dart
+++ lib/ui/widgets/app/app.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todos_repository/todos_repository.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final TodosRepository todosRepository;
+
+  const App({super.key, required this.todosRepository});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: todosRepository,
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
